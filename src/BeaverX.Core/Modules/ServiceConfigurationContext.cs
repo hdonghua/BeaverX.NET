c68339c2a48a38dca8ac211cf205dccdea012754
hdonghua <crackerwork@163.com>
2026-05-16 17:45:10 +0800

    增加配置获取

--- src/BeaverX.Core/Modules/ServiceConfigurationContext.cs
+++ src/BeaverX.Core/Modules/ServiceConfigurationContext.cs
@@ -1,5 +1,29 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace BeaverX.Core.Modules;
 
-public record ServiceConfigurationContext(IServiceCollection Services);
+public record ServiceConfigurationContext
+{
+    public IServiceCollection Services { get; }
+
+    public IConfiguration Configuration => _configurationLazy.Value;
+
+    private readonly Lazy<IConfiguration> _configurationLazy;
+    public ServiceConfigurationContext(IServiceCollection services)
+    {
+        Services = services;
+
+        _configurationLazy = new Lazy<IConfiguration>(() =>
+        {
+            var descriptor = services.FirstOrDefault(d => d.ServiceType == typeof(IConfiguration));
+
+            if (descriptor?.ImplementationInstance is IConfiguration configInstance)
+            {
+                return configInstance;
+            }
+
+            return new ConfigurationBuilder().Build();
+        });
+    }
+}
