626d08715b054122b0d303a147550d83956e0ca2
hdonghua <crackerwork@163.com>
2026-05-16 18:00:59 +0800

    实现接口自动注册示例

---
+++ src/samples/BeaverX.Sample.HttpApi/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using BeaverX.Sample.HttpApi.Services;
+using BeaverX.WebMvc.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeaverX.Sample.HttpApi.Controllers;
+
+public class TestController : BeaverXController
+{
+    private readonly MessageService messageService;
+
+    public TestController(MessageService messageService)
+    {
+        this.messageService = messageService;
+    }
+
+    [HttpGet]
+    public string SayHello(string nickname) => messageService.SayHello(nickname);
+}
---
+++ src/samples/BeaverX.Sample.HttpApi/Services/MessageService.cs
@@ -0,0 +1,8 @@
+﻿using BeaverX.Core.Dependency;
+
+namespace BeaverX.Sample.HttpApi.Services;
+
+public class MessageService : ITransientDependency
+{
+    public string SayHello(string nickname) => $"{nickname}, Hello";
+}
